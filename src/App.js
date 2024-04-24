5be8922085320f58c271ec18c40bde31b0e3c4c6
ingersonab <ingersonab24@mail.vmi.edu>
2024-04-23 23:47:37 -0400

    Homescreen and corresponding components created, elements added to hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './Pages/Home/Homescreen';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Home/>
     </div>
   );
 }
